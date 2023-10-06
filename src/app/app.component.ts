9e43ebe54bcff65cec4a6929658b4250069009f0
Dmytro Mazun <dimamazun1@gmail.com>
2023-10-06 22:55:24 +0300

    added form and select diagnose logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {DataService} from "./services/data.service";
+import {FormArray, FormBuilder} from "@angular/forms";
+import {debounceTime, mergeMap, Observable, startWith} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,92 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  form = this.fb.group({
+    encounter: this.fb.group({
+      date: ['']
+    }),
+    conditions: this.fb.array([])
+  });
+
+  get conditions() {
+    return this.form.get('conditions') as FormArray;
+  }
+
+  filteredDiagnoses: Observable<any[]>[] = [];
+  output: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private dataService: DataService
+  ) {
+  }
+
+  addCondition(e: Event): void {
+    e.preventDefault();
+
+    const condition = this.fb.group({
+      id: [''],
+      context: [],
+      name: [''],
+      code: [],
+      notes: [''],
+      onset_date: []
+    });
+
+    this.conditions.push(condition);
+    this.manageCondition(this.conditions.length - 1)
+  }
+
+  manageCondition(index: number): void {
+    const nameControl = this.conditions.at(index).get('name');
+
+    if (nameControl) {
+      this.filteredDiagnoses[index] = nameControl.valueChanges
+        .pipe(
+          startWith(''),
+          debounceTime(500),
+          mergeMap(value => this.dataService.getDiagnosesICPC2(value))
+        );
+    }
+  }
+
+  selectOption(condition: any, index: number) {
+    const newDiagnose = {
+      context: {
+        identifier: {
+          type: {
+            coding: [
+              {
+                system: "eHealth/resources",
+                code: "encounter"
+              }
+            ]
+          },
+          value: condition.id.toString()
+        }
+      },
+      code: {
+        coding: [
+          {
+            system: "eHealth/ICPC2/condition_codes",
+            code: condition.code
+          }
+        ]
+      },
+      onset_date: new Date().toISOString()
+    }
+
+    this.conditions.at(index).patchValue(newDiagnose);
+  }
+
+  generateData(e: Event): void {
+    e.preventDefault();
+    this.output = this.form.value;
+
+    if (this.output.conditions.length) {
+      this.output.conditions.map((c: any) => delete c.name);
+    } else {
+      delete this.output.conditions;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatInputModule} from "@angular/material/input";
+import {MatNativeDateModule} from "@angular/material/core";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClientModule} from "@angular/common/http";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
